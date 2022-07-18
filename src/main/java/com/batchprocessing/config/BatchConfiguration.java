38a3839f1db7bf1f10d070125879286dcb0ebd17
zaineb125 <zainebkefi520@gmail.com>
2022-07-18 13:55:06 +0100

    Parallel Steps

--- src/main/java/com/batchprocessing/config/BatchConfiguration.java
+++ src/main/java/com/batchprocessing/config/BatchConfiguration.java
@@ -3,6 +3,7 @@ package com.batchprocessing.config;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.Flow;
 
 import javax.sql.DataSource;
 
@@ -11,6 +12,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.ItemWriter;
@@ -29,6 +32,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -36,6 +41,7 @@ import com.batchprocessing.mapper.CustomerRowMapper;
 import com.batchprocessing.model.Customer;
 import com.batchprocessing.model.NewJobExecution;
 import com.batchprocessing.processor.CustomerItemProcessor;
+import com.batchprocessing.processor.CustomerItemProcessor2;
 import com.batchprocessing.repository.CustomerRepository;
 import com.batchprocessing.repository.NewJobExecutionRepository;
 
@@ -47,7 +53,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class BatchConfiguration {
 
-	//private Resource outputResource = new FileSystemResource("output/customer.csv");
+	private Resource outputResource = new FileSystemResource("output/customers.csv");
 	
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
@@ -66,48 +72,17 @@ public class BatchConfiguration {
 	
 	
 
-	//Cursor for Reading data from dataBase 
-	
-	/*@Bean
-	public JdbcCursorItemReader<Customer> reader(){
-		
-		JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<Customer>();
-		reader.setDataSource(dataSource);
-		reader.setSql("SELECT CustomerID,Genre,Age,Annual_Income,Spending_Score FROM customer");
-		reader.setRowMapper(new CustomerRowMapper());
-		
-		return reader ;
-	}*/
 
-	//Processor to process data 
+//**********************************************Step1***************************************************************************
 	
 	@Bean
-	public CustomerItemProcessor processor() {
-		
+	public CustomerItemProcessor processor1() {
 		return new CustomerItemProcessor();
 	}
-	/*
-	@Bean
-	public FlatFileItemWriter<Customer> writer() throws Exception{
-		
-		DBWriter dbWriter = new DBWriter(customerRepository);
-		dbWriter.writeInDB();
-		FlatFileItemWriter<Customer> writer = new FlatFileItemWriter<Customer>();
-		writer.setResource(outputResource);
-		
-		DelimitedLineAggregator<Customer> lineAggregator = new DelimitedLineAggregator<Customer>();
- 		lineAggregator.setDelimiter(",");
-		BeanWrapperFieldExtractor<Customer> fieldExtractor = new BeanWrapperFieldExtractor<Customer>();
-		fieldExtractor.setNames(new String[] {"CustomerID","Genre","Age","Annual_Income","Spending_Score"});
-		lineAggregator.setFieldExtractor(fieldExtractor);
-		
-		writer.setLineAggregator(lineAggregator);
-		return writer ; 
-	}
-	*/
+	
 	
 	@Bean
-	public ItemStreamReader<Customer> reader() {
+	public ItemStreamReader<Customer>reader1() {
 		JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<Customer>();
 		reader.setDataSource(dataSource);
 		reader.setSql("SELECT CustomerID,Genre,Age,Annual_Income,Spending_Score FROM customer");
@@ -115,37 +90,7 @@ public class BatchConfiguration {
 		return reader;
 	}
 	
-	@Bean
-	public ItemWriter<Customer> writer() {
-		JdbcBatchItemWriter<Customer> writer = new JdbcBatchItemWriter<Customer>();
-		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Customer>());
-		writer.setSql("INSERT INTO new_batch_job_execution (inserted_customerid,genre,age,annual_income,spending_score) VALUES (:CustomerID,:Genre,:Age,:Annual_Income,:Spending_Score)");
-		writer.setDataSource(dataSource);
-		return writer;
-	}
-	
-	@Bean
-	public Step step1(Customer customer) throws Exception {
-		return stepBuilderFactory.get("step1")
-								  .<Customer,Customer> chunk(10)
-								  .reader(reader())
-								  .processor(processor())
-								  .writer(writer())
-								  .build();
-								  
-	}
-	
-	@Bean
-	public Job exportCustomerJob(Customer customer) throws Exception {
-		
-		return jobBuilderFactory.get("exportCustomerJob")
-								.incrementer(new RunIdIncrementer())
-								.start(step1(customer))
-								.build();
-	}
-	
-	
-	/*public class CustomerRowMapper implements RowMapper<Customer>{
+	public class CustomerRowMapper implements RowMapper<Customer>{
 
 		@Override
 		public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -159,25 +104,136 @@ public class BatchConfiguration {
 			return customer ;
 		}
 		
-	}*/
+	}
 	
-	/*public class DBWriter implements ItemWriter<Customer> {
+	@Bean
+	public ItemWriter<Customer> writer1() {
+		JdbcBatchItemWriter<Customer> writer = new JdbcBatchItemWriter<Customer>();
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Customer>());
+		writer.setSql("INSERT INTO new_batch_job_execution (inserted_customerid,genre,age,annual_income,spending_score) VALUES (:CustomerID,:Genre,:Age,:Annual_Income,:Spending_Score)");
+		writer.setDataSource(dataSource);
+		return writer;
+	}
+	
+	@Bean
+	public Step step1(Customer customer) throws Exception {
+		System.out.println("************** Step1 initiated ***************");
+		return stepBuilderFactory.get("step1")
+								  .<Customer,Customer> chunk(10)
+								  .reader(reader1())
+								  .processor(processor1())
+								  .writer(writer1())
+								  .build();
+								  
+	}
+	
+	
+	
+	//**********************************************Step2***************************************************************************
+	
+	@Bean
+	public JdbcCursorItemReader<Customer> reader2(){
+		
+		JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<Customer>();
+		reader.setDataSource(dataSource);
+		reader.setSql("SELECT CustomerID,Genre,Age,Annual_Income,Spending_Score FROM customer");
+		reader.setRowMapper(new CustomerRowMapper());
+		
+		return reader ;
+	}
+	@Bean
+	public CustomerItemProcessor2 processor2() {
+		return new CustomerItemProcessor2();
+	}
+	@Bean
+	public FlatFileItemWriter<Customer> writer2() throws Exception{
+		FlatFileItemWriter<Customer> writer = new FlatFileItemWriter<Customer>();
+		writer.setResource(outputResource);
+		
+		DelimitedLineAggregator<Customer> lineAggregator = new DelimitedLineAggregator<Customer>();
+		lineAggregator.setDelimiter(",");
+		
+		BeanWrapperFieldExtractor<Customer> fieldExtractor = new BeanWrapperFieldExtractor<Customer>();
+		fieldExtractor.setNames(new String[] {"CustomerID","Genre","Age","Annual_Income","Spending_Score"});
+		lineAggregator.setFieldExtractor(fieldExtractor);
+		
+		writer.setLineAggregator(lineAggregator);
+		return writer ; 
+
+	}
+	
+	@Bean
+	public Step step2(Customer customer) throws Exception {
+		System.out.println("************** Step2 initiated ***************");
+		return stepBuilderFactory.get("step2")
+								  .<Customer,Customer> chunk(10)
+								  .reader(reader2())
+								  .processor(processor2())
+								  .writer(writer2())
+								  .build();
+								  
+	}
+	//*************************************************Flow*************************************************************************************
+	@Bean
+			public SimpleFlow splitFlow() throws Exception {
+			    return new FlowBuilder<SimpleFlow>("splitFlow")
+			        .split(taskExecutor())
+			        .add(flow1(),flow2())
+			        .build();
+			}
+		
+			@Bean
+			public SimpleFlow flow1() throws Exception {
+				System.out.println("************** Step1 Beguin ***************");
+				Customer customer =new Customer();
+			    return new FlowBuilder<SimpleFlow>("flow1")
+			        .start(step1(customer))
+			        .build();
+			}
+			@Bean
+			public SimpleFlow flow2() throws Exception {
+				System.out.println("**************Step2 Beguin ***************");
+				Customer customer =new Customer();
+			    return new FlowBuilder<SimpleFlow>("flow2")
+			        .start(step2(customer))
+			        .build();
+			}
+			
+		
+			@Bean
+			public TaskExecutor taskExecutor() {
+			    return new SimpleAsyncTaskExecutor("spring_batch");
+			}
+	//*****************************************************Job*********************************************************************************
+	
+	@Bean
+	public Job exportCustomerJob(Customer customer) throws Exception {
+	
+		return jobBuilderFactory.get("exportCustomerJob")
+								.start(splitFlow())
+								.build()
+						        .build();     
+	}
+	
+	
+	
+	//**************************************************WriterClass**************************************************
+	
+	public class DBWriter implements ItemWriter<Customer> {
 		
 		private NewJobExecutionRepository newJobExecutionRepository;
-		
-		
 		
 		@Autowired
 		public DBWriter(NewJobExecutionRepository newJobExecutionRepository) {
 			this.newJobExecutionRepository = newJobExecutionRepository;
 			
-		}*/
+		}
 		
 		
-		/*public void writeInDB()throws Exception{
+		public void writeInDB()throws Exception{
 			NewJobExecution newJobExecution =new NewJobExecution();
 			Customer customer = new Customer();
-			customer.setCustomerID("209");
+			customer.setCustomerID("210");
 			customer.setGenre("Male");
 			customer.setAge("22");
 			customer.setAnnual_Income("6000");
@@ -193,13 +249,13 @@ public class BatchConfiguration {
 			newJobExecution.setSpending_Score(customer.getSpending_Score());
 			newJobExecution.setInsertedCustomerID(customer.getCustomerID());
 			newJobExecutionRepository.save(newJobExecution);
-		}*/
+		}
 
 
-		/*@Override
+		@Override
 		public void write(List<? extends Customer> items) throws Exception {
 			writeInDB();
-		}*/
+		}
 		
 	}
-
+}
--- src/main/java/com/batchprocessing/processor/CustomerItemProcessor2.java
+++ src/main/java/com/batchprocessing/processor/CustomerItemProcessor2.java
@@ -4,15 +4,13 @@ import org.springframework.batch.item.ItemProcessor;
 
 import com.batchprocessing.model.Customer;
 
-public class CustomerItemProcessor implements ItemProcessor<Customer , Customer> {
-
+public class CustomerItemProcessor2 implements ItemProcessor<Customer , Customer>  {
 	@Override
 	public Customer process(Customer customer) throws Exception {
-		System.out.println("Step1");
+		System.out.println("Step2");
 		System.out.println("Reading from database Id :  "+ customer.getCustomerID());
 		System.out.println("Reading from database Genre :  "+ customer.getGenre());
 		System.out.println("Reading from database Genre :  "+ customer.getGenre());
 		return customer;
 	}
-
 }
